15025631de630d0ea5872b2b75562e4e58670b59
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-05-04 15:18:08 -0600

    Add server test and setup

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/', function(request, response) {
   response.send('The front page works.');
 });
 
-app.listen(app.get('port'), function() {
-  console.log("App is running!");
-});
+if (!module.parent) {
+  app.listen(app.get('port'), function() {
+    console.log("App is running!");
+  });
+};
+
+module.exports = app;
---
+++ test/server-test.js
@@ -0,0 +1,20 @@
+const assert = require('chai').assert;
+const app = require('../server');
+
+describe('Server', function() {
+  before(function(done){
+    this.port = 9876;
+    this.server = app.listen(this.port, function(err, result) {
+      if (err) { return done(err); }
+      done();
+    });
+  });
+
+  after(function() {
+    this.server.close();
+  });
+
+  it('should exist', function() {
+    assert(app);
+  });
+});
