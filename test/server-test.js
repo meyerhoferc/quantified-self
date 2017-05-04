966b21f9cfe29b1febb365eaa84ca5d2049275fb
Andy Wooten <andrewdwooten@icloud.com>
2017-05-04 15:39:12 -0600

    Add api endpoint and sadpath test
    Adds api endpoint at /api/foods/:id to retrieve a food
    object. Sad path test added to server-test.js which
    asserts 404 if unable to locate food. Test is currently
    passing. Adds 'request' node module for request testing.
    All tests currently passing.

--- test/server-test.js
+++ test/server-test.js
@@ -1,5 +1,7 @@
 const assert = require('chai').assert;
 const app = require('../server');
+const request = require('request');
+
 
 describe('Server', function() {
   before(function(done){
@@ -7,6 +9,9 @@ describe('Server', function() {
     this.server = app.listen(this.port, function(err, result) {
       if (err) { return done(err); }
       done();
+    });
+    this.request = request.defaults({
+      baseUrl: 'http://localhost:9876/'
     });
   });
 
@@ -16,5 +21,20 @@ describe('Server', function() {
 
   it('should exist', function() {
     assert(app);
+  });
+
+  describe('GET api/foods/:id', function(){
+    beforeEach(function(){
+      app.locals.secrets = {
+        1: {name: 'banana', calories: 50}
+      }
+    });
+    it('should return a 404 if food not found', function(done){
+      this.request.get('/api/foods/2', function(error, response){
+        if(error){ done(error); }
+        assert.equal(response.statusCode, 404);
+        done();
+      });
+    });
   });
 });
